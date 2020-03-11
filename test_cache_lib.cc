3580ca71e63ebbff859f9ee1b0951db5c8cfa52a
enderquestral <hannahhellerstein@gmail.com>
2020-03-10 20:21:44 -0700

    Updated test file

--- test_cache_lib.cc
+++ test_cache_lib.cc
@@ -1,8 +1,11 @@
+#define CATCH_CONFIG_MAIN
+
 #include "evictor.hh"
 #include "lru_evictor.hh"
 #include "fifo_evictor.hh"
 #include "cache.hh"
 #include <cassert>
+#include "catch.hpp"
 
 /*For testing purposes, assume the data payload is always of type C string, and don't forget to account for the last '\0' character 
 at the end of the line. It will be easier to debug and print data of this type.
@@ -17,8 +20,219 @@ int return_number(key_type str){
 
 function<size_t(key_type)> myhasher = return_number;  //takes str turns it into its str size
 
+/*
+No interdependencies between tests.
+No stateful tests--each test is standalone. Your test suite should use a single (global) Cache object, but reset it at the end or beginning of each test.
+One piece of functionality tested per test (function). You can extract common set up code to a fixture object.
+"Black box" testing only: no testing of internal functionality, optional parameters, or implementation details.
+*/
 
-int main(){
+
+//auto FIFO = Fifo_Evictor();
+
+
+//auto cache2 = Cache(40, 0.75, FIFO, myhasher);
+//Testing for cache.hh
+
+
+
+TEST_CASE("Testing that Set works"){
+    //Gonna have to assume that get works here
+    auto cache = Cache(40, 0.75, nullptr, myhasher);
+    Cache::size_type size;
+    Cache::val_type val1 = "value_1";
+    Cache::val_type val2 = "value_2alright";
+    Cache::val_type valr = "reject?";
+    Cache::val_type val3 = "Floccinaucinihilipilification";  //It's 29 characters!
+
+
+    SECTION("Can add a key-value pair"){
+        cache.set("key1", val1, 8);
+        REQUIRE(*cache.get("key1", size) == *val1);
+    }
+    SECTION("Overwrites old value if key exists"){
+        cache.set("key1", val1, 8);
+        cache.set("key1", val2, 16);
+        REQUIRE(*cache.get("key1", size) == *val2); 
+    }
+    SECTION("If maxmem is exceeded, new value is not inserted"){
+        //Evictor is tested elsewhere as by implication from cache.hh, once cache is created evictor cannot be reassigned.
+        cache.set("key1", val3, 30);
+        cache.set("key2", valr, 8);
+        REQUIRE(*cache.get("key1", size) == *val3);
+        REQUIRE(*cache.get("key2", size) == *valr);
+        cache.set("key3", val2, 16); //Should not get inserted
+        REQUIRE(cache.get("key3", size) == nullptr);
+        
+    }
+    cache.reset();
+    //Instructions said to reset after every test, but... reset is a function?
+}
+
+TEST_CASE("Testing that Get works"){
+    //We somewhat Need to use set/be interdependent on set for part of Get, since set set is how we add things to cache
+    auto cache = Cache(40, 0.75, nullptr, myhasher);
+    Cache::size_type size;
+    Cache::val_type val1 = "value_1";
+    Cache::val_type val2 = "value_2alright";
+    Cache::val_type valr = "reject?";
+
+    SECTION("Getting nullptr when trying to access nothing"){ //
+        REQUIRE(cache.get("anykey", size) == nullptr);
+    }
+    SECTION("Getting correct value from cache of size 1"){
+        cache.set("key1", val1, 8);
+        REQUIRE(*cache.get("key1", size) == *val1);
+    }
+    SECTION("Getting correct value from cache of multiple objects"){
+        cache.set("key1", val1, 8);
+        cache.set("key2", val2, 16);
+        cache.set("key3", valr, 8);
+        REQUIRE(*cache.get("key2", size) == *val2);
+        REQUIRE(*cache.get("key1", size) == *val1);
+        REQUIRE(*cache.get("key3", size) == *valr); 
+
+    }
+    SECTION("size_type size is getting changed"){
+        cache.set("key1", val1, 8);
+        cache.get("key1", size);
+        auto sizebefore = size;
+        cache.set("key2", val2, 16);
+        cache.get("key2", size);
+        REQUIRE(sizebefore != size);
+
+    }
+    cache.reset();
+}
+
+
+
+
+TEST_CASE("Testing that SpaceUsed works"){
+    auto cache = Cache(40, 0.75, nullptr, myhasher);
+    Cache::val_type val1 = "value_1";
+    Cache::val_type val2 = "value_2alright";
+    Cache::val_type valr = "reject?";
+
+    SECTION("Empty cache has spaceused size of 0"){
+        REQUIRE(cache.space_used() == 0);
+    }
+    SECTION("Returns sum of size of one value cache"){
+        cache.set("key1", val1, 8);
+        REQUIRE(cache.space_used() == 8);
+    }
+    SECTION("Returns sum of size of all values"){
+        cache.set("key1", val1, 8);
+        cache.set("key2", val2, 16);
+        cache.set("keyr", valr, 8);
+        REQUIRE(cache.space_used() == 32);
+    }
+
+    cache.reset();
+}
+
+TEST_CASE("Testing that Del works"){
+    auto cache = Cache(40, 0.75, nullptr, myhasher);
+    Cache::size_type size;
+    Cache::val_type val1 = "value_1";
+    Cache::val_type val2 = "value_2alright";
+    Cache::val_type valr = "reject?";
+
+    SECTION("Returns false if attempting to delete something from empty cache"){
+        REQUIRE(cache.del("key1") == false);
+    }
+    SECTION("Attempt to delete something that is not in cache when has other things in it"){
+        cache.set("key1", val1, 8);
+        cache.set("key2", val2, 16);
+        cache.set("keyr", valr, 8);
+        REQUIRE(cache.del("key3") == false);
+    }
+    SECTION("Successfully delete something that is in the cache"){
+        cache.set("key1", val1, 8);
+        cache.set("key2", val2, 16);
+        cache.set("keyr", valr, 8);
+        auto boolresult = cache.del("keyr");
+        REQUIRE(boolresult == true);
+        REQUIRE(cache.get("keyr", size) == nullptr);
+        REQUIRE(cache.space_used() != 32);
+
+    }
+    SECTION("Attempt to delete something that has already been deleted"){
+        //Should return false
+        cache.set("key1", val1, 8);
+        cache.set("key2", val2, 16);
+        cache.set("keyr", valr, 8);
+        cache.del("keyr");
+        REQUIRE(cache.del("keyr") == false);
+    }
+    cache.reset();
+}
+TEST_CASE("Testing that reset works"){
+    auto cache = Cache(40, 0.75, nullptr, myhasher);
+    Cache::size_type size;
+    Cache::val_type val1 = "value_1";
+    Cache::val_type val2 = "value_2alright";
+    Cache::val_type valr = "reject?";
+    cache.set("key1", val1, 8);
+    cache.set("key2", val2, 16);
+    cache.set("keyr", valr, 8);
+    REQUIRE(cache.space_used() != 0);
+    cache.reset();
+    REQUIRE(cache.space_used() == 0);
+    REQUIRE(cache.get("key2", size) == nullptr);
+    REQUIRE(cache.get("key1", size) == nullptr);
+    REQUIRE(cache.get("keyr", size) == nullptr);    
+
+}
+
+//Testing for evictor.hh functionality
+
+//Note: Below case specifically for FIFO
+TEST_CASE("Testing for Touch: FIFO"){
+    auto FIFO = Fifo_Evictor();
+    auto cache = Cache(40, 0.75, &FIFO, myhasher);
+    SECTION("Touch happened in Get"){
+
+    }
+    SECTION("Touch happened in Set"){
+
+    }
+
+
+
+}
+
+TEST_CASE("Testing for Evict: FIFO"){
+    auto FIFO = Fifo_Evictor();
+    auto cache = Cache(40, 0.75, &FIFO, myhasher); 
+
+    
+}
+
+//Note: Below case specifically for FIFO
+TEST_CASE("Testing for Touch: LRU"){
+    auto LRU = LRU_Evictor();
+    auto cache = Cache(40, 0.75, &LRU, myhasher);
+    SECTION("Touch happened in Get"){
+
+    }
+    SECTION("Touch happened in Set"){
+
+    }
+
+
+
+}
+
+TEST_CASE("Testing for Evict: LRU"){
+    auto LRU = LRU_Evictor();
+    auto cache = Cache(40, 0.75, &LRU, myhasher); 
+
+    
+}
+
+
+/*int main(){
     auto cache = Cache(40, 0.75, nullptr, myhasher);
     auto defaultedcache = Cache(32); //has no given hasher, uses its own default
 
@@ -26,16 +240,13 @@ int main(){
     Cache::size_type size;
     Cache::val_type val1 = "value_1";
     Cache::val_type val2 = "value_2alright";
-    //Cache::val_type val3 = "value3";
     Cache::val_type valr = "reject?";
-    //Cache::val_type val4 = "value4";
-    //Cache::val_type val5 = "value5";
 
     assert(cache.space_used()==0);
     cache.set("key1", val1, 8);
     cache.set("key2", val2, 16);
     cache.set("key3", valr, 8); //Maybe reject this.
-    //cache.set("key4", "reject", 8); //Almost certianly reject this
+
     //test get
     assert(*cache.get("key1", size) == *val1);
     assert(*cache.get("key2", size) == *val2);
@@ -70,27 +281,7 @@ int main(){
     
     // Like the above, the following will only compile if pImpl_ is made public.
     auto small_cache = Cache(50, 0.75);
-    /*
-    cout << "Start: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k1", "s_v1", 5);
-    cout << "1 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k2", "s_v2", 5);
-    assert(small_cache.pImpl_->map.bucket_count() == 5);
-    cout << "2 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k3", "s_v3", 5);
-    assert(small_cache.pImpl_->map.bucket_count() > 5);
-    cout << "3 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k4", "s_v4", 5);
-    cout << "4 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k5", "s_v5", 5);
-    cout << "5 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    small_cache.set("s_k6", "s_v6", 5);
-    assert(small_cache.pImpl_->map.bucket_count() == 11);
-    small_cache.set("s_k7", "s_v7", 5);
-    cout << "7 K-V: " << small_cache.pImpl_->map.bucket_count() << endl;
-    assert(small_cache.pImpl_->map.bucket_count() > 11);
-    small_cache.set("s_k8", "s_v8", 5);
-    */
+
 
     
 
@@ -191,4 +382,4 @@ int main(){
     
     //small_cache.reset();
     return 0;
-}
+}*/
